366eefddba10af87fd1b34c004dc2d7191f46d3a
NSticki <maletinseregy55@gmail.com>
2025-03-30 02:22:26 +0700

    ChangeScene
    переключатель

--- Assets/TapToChangeScene.cs
+++ Assets/TapToChangeScene.cs
@@ -1,18 +1,35 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class TapToChangeScene : MonoBehaviour
 {
-    public string nextSceneName = ""; // следующая сцены
+    public string nextSceneName = ""; // Название следующей сцены
+    public Button changeSceneButton; // Кнопка для смены сцены
+
+    void Start()
+    {
+        if (changeSceneButton != null)
+        {
+            changeSceneButton.onClick.AddListener(ChangeScene);
+        }
+    }
 
     void Update()
     {
-   
         if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began)
         {
-            
             SceneManager.LoadScene(nextSceneName);
         }
-
     }
+
+    public void ChangeScene()
+    {
+        SceneManager.LoadScene("MAP");
+        SceneManager.LoadScene("TASK");
+        SceneManager.LoadScene("CHOOSEBUSINES");
+    }
+
+  
+
 }
