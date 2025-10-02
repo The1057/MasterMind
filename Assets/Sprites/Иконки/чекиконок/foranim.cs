4e357f7231ad6ce7db6eea0e9c7cb851d6135762
1L0L111 <kurandin2004@bk.ru>
2025-10-02 19:19:33 +0700

    да, 178, это просто секретная сцена

---
+++ Assets/Sprites/Иконки/чекиконок/foranim.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+
+public class foranim: MonoBehaviour
+{
+    public Animator animator;
+    public void for1()
+    {
+        animator.SetTrigger("F1");
+    }
+    public void for2()
+    {
+        animator.SetTrigger("F2");
+    }
+    public void for3()
+    {
+        animator.SetTrigger("F3");
+    }
+    public void for4()
+    {
+        animator.SetTrigger("F4");
+    }
+    public void for5()
+    {
+        animator.SetTrigger("F5");
+    }
+}
